bdbaec156c07e5545148ba199d4efa1f912f2e02
Yurii Chepets <ychepets@gmail.com>
2022-05-12 11:50:23 +0300

    IDEA code  formatting applied

--- src/SalariedEmployee.java
+++ src/SalariedEmployee.java
@@ -1,29 +1,29 @@
 public class SalariedEmployee extends Employee {
-  private String socialSecurityNumber;
-  private double fixedMonthlyPayment;
+    private String socialSecurityNumber;
+    private double fixedMonthlyPayment;
 
-  public SalariedEmployee(String employeeId, String name, String socialSecurityNumber) {
-    super(employeeId, name);
-    this.socialSecurityNumber = socialSecurityNumber;
-  }
+    public SalariedEmployee(String employeeId, String name, String socialSecurityNumber) {
+        super(employeeId, name);
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
 
-  public String getSocialSecurityNumber() {
-    return socialSecurityNumber;
-  }
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
 
-  public void setSocialSecurityNumber(String socialSecurityNumber) {
-    this.socialSecurityNumber = socialSecurityNumber;
-  }
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
 
-  public double getFixedMonthlyPayment() {
-    return fixedMonthlyPayment;
-  }
+    public double getFixedMonthlyPayment() {
+        return fixedMonthlyPayment;
+    }
 
-  public void setFixedMonthlyPayment(double fixedMonthlyPayment) {
-    this.fixedMonthlyPayment = fixedMonthlyPayment;
-  }
+    public void setFixedMonthlyPayment(double fixedMonthlyPayment) {
+        this.fixedMonthlyPayment = fixedMonthlyPayment;
+    }
 
-  public double calculatePay() {
-    return fixedMonthlyPayment;
-  }
+    public double calculatePay() {
+        return fixedMonthlyPayment;
+    }
 }
